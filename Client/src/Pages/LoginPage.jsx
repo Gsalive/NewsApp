1cf18e903c1c5cb1daed0e610e891f44f024132b
Gsalive <gsalive2002@gmail.com>
2023-08-18 04:33:54 +0530

    adding homepage

--- Client/src/Pages/LoginPage.jsx
+++ Client/src/Pages/LoginPage.jsx
@@ -22,7 +22,7 @@ export default function Login() {
 
     async function loginuser(event){
         event.preventDefault();
-      const response= await fetch('http://localhost:1337/api/login',{
+      const response= await fetch('https://catch-of-the-day-c8ah.onrender.com/api/login',{
         method:'POST',
         headers:{
             'Content-Type':'application/json',
